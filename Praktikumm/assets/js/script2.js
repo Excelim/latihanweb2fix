10b788bd9ff690765595107fbd26810523fb58a2
Excelim <127103742+Excelim@users.noreply.github.com>
2023-10-12 23:27:24 +0700

    Add files via upload

---
+++ Praktikumm/assets/js/script2.js
@@ -0,0 +1,12 @@
+    function validateAndSubmit() {
+        var nama = document.getElementById('nama').value;
+        var email = document.getElementById('email').value;
+        var alamat = document.getElementById('alamat').value;
+
+        if (nama === "" || email === ""  || alamat === "") {
+            alert("Anda harus mengisi data dengan lengkap");
+        } else {
+            document.getElementById('registrationForm').submit();
+        }
+    }
+
